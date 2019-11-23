b6870e77475f802ce9947b48a1b629f274407d47
ymk1t <yuma1102eff@gmail.com>
2019-11-23 22:11:55 +0900

    特徴量ベクトルの結合

---
+++ mecab/feature_union.py
@@ -0,0 +1,21 @@
+import scipy
+from sklearn.feature_extraction.text import CountVectorizer
+
+from tokenizer import tokenize
+
+texts = [
+    '私は私のことを好きなあなたが好きです',
+    '私はラーメンが好きです',
+    '富士山は日本一高い山です',
+]
+
+word_bow_vectorizer = CountVectorizer(tokenizer=tokenize)
+char_bigram_vectorizer = CountVectorizer(analyzer='char', ngram_range=(2, 2))
+
+word_bow_vectorizer.fit(texts)
+char_bigram_vectorizer.fit(texts)
+
+word_bow = word_bow_vectorizer.transform(texts)
+char_bigram = char_bigram_vectorizer.transform(texts)
+
+feat = scipy.sparse.hstack((word_bow, char_bigram))
